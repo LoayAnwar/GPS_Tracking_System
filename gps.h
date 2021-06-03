459ae6b10acb1bf789be3191cbfc697dd44df503
LinaAyman31 <77002601+LinaAyman31@users.noreply.github.com>
2021-06-03 23:56:37 +0200

    main

--- gps.h
+++ gps.h
@@ -1,3 +1,8 @@
+#ifndef DIO_DRIVER_H_
+#define DIO_DRIVER_H_
+
 char ReadData(void);
 void  read_gps_data();
 double degree_to_rad(double degree);
+
+#endif
--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include "tm4c123gh6pm.h"
-//#include "DIO_DRIVER.h"
+#include "DIO_DRIVER.h"
+#include "gps.h"
 //#include "sysTick.h"
+
 void SystemInit()
 {
 	
 }
 int __main (void)
 {
-
+ 
+	if(total_distance > =100){
+	    Portf_output(0x02);
+   }
+	
 }
