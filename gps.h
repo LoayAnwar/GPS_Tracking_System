deba672167d4c22135f74fd04ed2eec76e949c8f
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-06-04 00:15:46 +0200

    add read from GPS and compute distance in main

--- gps.h
+++ gps.h
@@ -2,7 +2,7 @@
 #define DIO_DRIVER_H_
 
 char ReadData(void);
-void  read_gps_data();
+void read_gps_data();
 float get_latitude(unsigned char lat_pointer);
 float get_longitude(unsigned char lon_pointer);
 double degree_to_rad(double degree);
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-
+#include <stdlib.h>
+#include <stdbool.h>
 #include "tm4c123gh6pm.h"
 #include "DIO_DRIVER.h"
 #include "gps.h"
@@ -10,7 +11,41 @@ void SystemInit()
 }
 int __main (void)
 {
- 
+ 	 float prev_lat;
+   float prev_lon;
+   float current_lat;
+   float current_lon;
+   float distance = 0;
+   float total_distance = 0;
+   bool first_read = true;
+		
+	// **************** Read data from gps and compute distance ***************
+	while (1) {
+        read_gps_data();
+        current_lat = get_latitude(data_start[0]);
+        current_lon = get_longitude(data_start[2]);
+
+        if (is_N_or_S == 'S') {
+            current_lat *= -1;
+        }
+        if (is_E_or_W == 'W') {
+            current_lon *= -1;
+        }
+				
+        if (first_read) {
+            prev_lat = current_lat;
+            prev_lon = current_lon;
+            first_read = false;
+        }
+				// get distance in miles
+        distance = get_distance(current_lat, current_lon, prev_lat, prev_lon);
+				// convert distance from miles to km
+        distance = distance * 1.609344;
+        total_distance += distance;
+        prev_lat = current_lat;
+        prev_lon = current_lon;
+    }
+	
 	if(total_distance > =100){
 	    Portf_output(0x02);
    }
