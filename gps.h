c53b8db6d820639a56cf5b9b04c9980e116df287
LinaAyman31 <77002601+LinaAyman31@users.noreply.github.com>
2021-06-04 00:06:55 +0200

    gps.h

--- gps.h
+++ gps.h
@@ -3,6 +3,8 @@
 
 char ReadData(void);
 void  read_gps_data();
+float get_latitude(unsigned char lat_pointer);
+float get_longitude(unsigned char lon_pointer);
 double degree_to_rad(double degree);
 
 #endif
