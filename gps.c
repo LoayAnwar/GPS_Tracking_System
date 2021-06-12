87afa36b4c7a26b90b691ca2561a68447d7a37c2
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-06-12 13:44:18 +0200

    bug fix in gps.c

--- gps.c
+++ gps.c
@@ -9,35 +9,35 @@
 #define buffer_size 80
 #define pi 3.14159265358979323846
                       
-uint8_t CommaCounter;
-volatile unsigned int buffer_index;
-volatile unsigned char  is_GGA = 0;
-char buffer[buffer_size];              
-char GGA[3];
-char data_start[20]; 
 
-float current_lon;
-float current_lat;
-float prev_lon;
-float prev_lat;
-unsigned char is_N_or_S;
-unsigned char is_E_or_W;
-float distance;
-unsigned int total_distance = 0;
 
-float get_latitude(char lat_pointer);
-void compute_lat_lon();
-float get_longitude(char lon_pointer);
+
+             
+
+
+
+
+
+
+long double get_latitude(char *buffer, unsigned char lat_pointer);
+unsigned int compute_lat_lon(char *data_start, char *buffer);
+long double get_longitude(char *buffer, unsigned char lon_pointer);
 double get_distance(double current_lat, double current_lon, double prev_lat, double prev_lon);
-bool first = true;
 
-int i = 0;
 
 void read_gps_data()
 {
-	char received_char;		
+		unsigned int total_distance = 0;
+		char buffer[buffer_size]; 
+		char received_char;		
 	//received_char = ReadData_UART0();
-    CommaCounter = 0;
+		uint8_t CommaCounter = 0;
+		volatile unsigned int buffer_index;
+		uint8_t  is_GGA = 0;
+		char GGA[3];
+		char data_start[20]; 
+
+
     is_GGA = 0;
     do {
         //received_char = ReadData_UART0();
@@ -67,19 +67,24 @@ void read_gps_data()
             GGA[2] = received_char;
         }
     } while (CommaCounter != 13);
-		compute_lat_lon();
+		total_distance = compute_lat_lon(data_start, buffer);
 }
 
 //****************** compute lat and lon from gps *********
-void compute_lat_lon() {
-	current_lat = get_latitude (data_start[0]);
-	current_lon = get_longitude (data_start[2]);
-	if (is_N_or_S == 'S') {
-		current_lat *= -1;
-	}
-	if (is_E_or_W == 'W') {
-		current_lon *= -1;
-	}
+unsigned int compute_lat_lon(char *data_start, char *buffer) {
+	long double current_lon;
+	long double current_lat;
+	long double prev_lon;
+	long double prev_lat;
+	long double distance;
+	bool first = true;
+	unsigned int total_distance = 0;
+ 	unsigned char data_begin = data_start[0];
+	current_lat = get_latitude (buffer ,data_begin);
+	data_begin = data_start[2];
+	current_lon = get_longitude (buffer, data_begin);
+	
+	
 	if (first) {
 		prev_lat = current_lat;
 		prev_lon = current_lon;
@@ -95,43 +100,55 @@ void compute_lat_lon() {
 	
 	prev_lat = current_lat;
 	prev_lon = current_lon;
+	
+	return total_distance;
 }
 
 // ***************** get latitude value ******************
-float get_latitude(char lat_pointer)
+long double get_latitude(char *buffer ,unsigned char lat_pointer)
 {
+		char *ptr;
+		unsigned char is_N_or_S;
     unsigned char lat_index = lat_pointer + 1;    
     unsigned char index = 0;
     char lat_buffer[15];
-    float latitude;
-    memset(lat_buffer, 0, 15);
+    long double latitude;
+   // memset(lat_buffer, 0, 15);
     for (; buffer[lat_index] != ','; lat_index++) {
         lat_buffer[index] = buffer[lat_index];
         index++;
     }
     lat_index++;
     is_N_or_S = buffer[lat_index];
-    latitude = atof(lat_buffer); 
+    latitude = strtold(lat_buffer, &ptr); 
+		if (is_N_or_S == 'S') {
+			latitude *= -1;
+		}
 		return latitude;
 }
 
 //****************** get longitude ************************
-float get_longitude(char lon_pointer)
+long double get_longitude(char *buffer, unsigned char lon_pointer)
 {
+		char *ptr;
+		unsigned char is_E_or_W;
     unsigned char lon_index = lon_pointer + 1;
     unsigned char index;       
     char long_buffer[15];
     float longitude;
     lon_index = 0;
-    memset(long_buffer, 0, 15);
+   // memset(long_buffer, 0, 15);
     for (; buffer[lon_pointer] != ','; lon_pointer++) {
         long_buffer[index] = buffer[lon_pointer];
         index++;
     }
     lon_index++;
     is_E_or_W = buffer[lon_index];
-    longitude = atof(long_buffer);
-return longitude;    
+    longitude = strtold(long_buffer, &ptr);
+		if (is_E_or_W == 'W') {
+			longitude *= -1;
+		}
+	return longitude;    
 }
 
 
--- gps.h
+++ gps.h
@@ -2,8 +2,8 @@
 #define gps_H_
  
 void read_gps_data();
-void get_latitude(int lat_pointer ,char * is_N_or_S ,double* l);
-void get_longitude(int lon_pointer  ,char* is_E_or_W ,double* l);
-void get_distance(double current_lat, double current_lon, double prev_lat, double prev_lon ,double* distance);
+//void get_latitude(int lat_pointer ,char * is_N_or_S ,double* l);
+//void get_longitude(int lon_pointer  ,char* is_E_or_W ,double* l);
+//void get_distance(double current_lat, double current_lon, double prev_lat, double prev_lon ,double* distance);
 
 #endif
