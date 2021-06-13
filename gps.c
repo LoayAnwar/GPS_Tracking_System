2bb2a928ea8b4e6ccb7d6a88d48a022a899cd1fe
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-06-13 10:00:42 +0200

    bug fix

--- gps.c
+++ gps.c
@@ -102,19 +102,19 @@ void read_gps_data()
 
 //****************** compute lat and lon from gps *********
 void compute_lat_lon() {
-	long double temp;
+	int temp;
 	long double temp_2;
 	long double distance;
  	unsigned char data_begin = data.data_start[0];
 	char ay = data.buffer[0];
 	data.current_lat = get_latitude(data_begin);
-	temp = (int)data.current_lat/100;
+	temp = data.current_lat/100;
 	temp_2 = data.current_lat - (temp * 100);
 	temp_2 = temp_2 / 60;
 	data.current_lat = temp + temp_2;
 	data_begin = data.data_start[2];
 	data.current_lon = get_longitude (data_begin);
-	temp = (int)data.current_lon / 100;
+	temp = data.current_lon / 100;
 	temp_2 = data.current_lon - (temp * 100);
 	temp_2 /= 60;
 	data.current_lon = temp + temp_2;
