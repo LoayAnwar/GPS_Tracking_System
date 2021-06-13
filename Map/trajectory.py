9c243d99f1cdd43d36c566d5ed33d61863da3687
LailaWael3 <77001864+LailaWael3@users.noreply.github.com>
2021-06-13 13:28:27 +0200

    read from text file

--- Map/trajectory.py
+++ Map/trajectory.py
@@ -1,18 +1,19 @@
 from branca import colormap
 import folium
 
-latitude = [30.288633346557617, 30.290264129638672, 31.273269653320316, 31.25301361083984]
-longitude = [30.076298837912606, 30.084394409909613, 30.086028298208685, 30.08231487667201]
+f = open("test2.txt", "r")
+reading= f.read().split(',')
 
 #Map object
-map_object = folium.Map(location=[latitude[0], longitude[0]], zoom_start=15)
+map_object = folium.Map(location=[reading[0], reading[1]], zoom_start=15)
 
 #Markers for start and end locations
-folium.Marker(location=[latitude[0], longitude[0]], icon=folium.Icon(color='red')).add_to(map_object),
-folium.Marker(location=[latitude[-1], longitude[-1]], icon=folium.Icon(color='red')).add_to(map_object)
+folium.Marker(location=[reading[0], reading[1]], icon=folium.Icon(color='red')).add_to(map_object),
+folium.Marker(location=[reading[-2], reading[-1]], icon=folium.Icon(color='red')).add_to(map_object)
 
-for i in range(len(latitude)):
-    folium.CircleMarker([latitude[i], longitude[i]],
+#plotting points
+for i in range(0, len(reading)-1, 2):
+    folium.CircleMarker([reading[i], reading[i+1]],
                         fill = True,
                         color = 'blue',
                         radius = 10,
