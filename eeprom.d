06158f455d03782a3b3c1f0eac8fbade6b1a7e52
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-06-13 16:38:52 +0200

    uart2 init added

--- eeprom.d
+++ eeprom.d
@@ -1,6 +1,6 @@
 .\eeprom.o: EEPROM.c
 .\eeprom.o: tm4c123gh6pm.h
-.\eeprom.o: F:\ARM\ARMCC\bin\..\include\stdio.h
-.\eeprom.o: F:\ARM\ARMCC\bin\..\include\stdlib.h
-.\eeprom.o: F:\ARM\ARMCC\bin\..\include\stdbool.h
-.\eeprom.o: F:\ARM\ARMCC\bin\..\include\string.h
+.\eeprom.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\eeprom.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\eeprom.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\eeprom.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
---
+++ uart2.h
@@ -0,0 +1,8 @@
+#ifndef UART_H_
+#define UART2_H_
+#include "stdint.h"
+
+void UART2_Init(void);
+uint8_t ReadData_uart2(void);
+void SendData_UART2(char data);
+#endif
